6a153c3c8494ba2a46ce8b0183fbe40d62089cf6
vineetkatiyar98 <104678201+vineetkatiyar98@users.noreply.github.com>
2024-03-08 13:27:03 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+     images : {
+          remotePatterns : [
+               {
+                    hostname : "o7poiebcqopeuadz.public.blob.vercel-storage.com"
+               }
+          ]
+     },
+     
+};
+
+module.exports = nextConfig;
